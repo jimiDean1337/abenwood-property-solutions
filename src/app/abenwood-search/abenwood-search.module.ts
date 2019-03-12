f5510f05156c0501868ff14e9a2e2f2ed15e7c79
jmbofly <flynn-31199@cotc.edu>
2019-03-12 19:25:45 -0400

    added to shared module

--- src/app/abenwood-search/abenwood-search.module.ts
+++ src/app/abenwood-search/abenwood-search.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-// import { SharedModule } from '../shared/shared.module';
 import { AbenwoodSearchComponent } from './abenwood-search.component';
 
 @NgModule({
   declarations: [AbenwoodSearchComponent],
-  imports: [
-    // SharedModule
-  ],
   exports: [AbenwoodSearchComponent],
   entryComponents: [AbenwoodSearchComponent],
 })
