7fbdaee5589b5ee52f913bbdc4e0293d87d49dac
jmbofly <flynn-31199@cotc.edu>
2019-03-12 19:27:21 -0400

    minor code refactor

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -8,25 +8,24 @@ import { Property } from './property';
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.scss']
+  styleUrls: ['./properties.component.scss'],
 })
 export class PropertiesComponent implements OnInit {
-	list$: Observable<Property[]>;
-  constructor(public router: Router, public route: ActivatedRoute) { }
+  list$: Observable<Property[]>;
+  constructor(public router: Router, public route: ActivatedRoute) {}
 
   ngOnInit() {
-  	this.list$ = new Observable(obs => {
-  		obs.next(MockProperties)
-  	})
-		// this.list$ = this.route.paramMap.pipe(
-		// 	switchMap((params: ParamMap) =>
-		// 		this.propertyService.getList(params.get('id'))
-		// 		)
-		// )
+    this.list$ = new Observable(obs => {
+      obs.next(MockProperties);
+    });
+    // this.list$ = this.route.paramMap.pipe(
+    // 	switchMap((params: ParamMap) =>
+    // 		this.propertyService.getList(params.get('id'))
+    // 		)
+    // )
   }
 
-  viewProperty(id) {
-		this.router.navigateByUrl(`properties/${id}`);
+  viewProperty(id: number) {
+    this.router.navigateByUrl(`properties/${id}`);
   }
-
 }
